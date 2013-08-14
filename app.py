743bbacc53f29eb345f34b01be408e5f3ff423c0
Daniel Zappala <daniel.zappala@gmail.com>
2013-08-14 12:52:32 -0600

    First cut at front page design.

---
+++ app.py
@@ -0,0 +1,32 @@
+import sys
+
+from flask import Flask, render_template
+from flask_frozen import Freezer
+
+app = Flask(__name__)
+freezer = Freezer(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html',active='index')
+
+@app.route('/<directory>',defaults={'page':''})
+@app.route('/<directory>/<page>')
+def show(directory,page):
+    print page
+    if not page:
+        return render_template(directory+'.html',active=directory)
+    print directory
+    return render_template(directory+"/" + page + '.html',active=directory)
+    
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.debug = True
+        app.run(port=8080)
